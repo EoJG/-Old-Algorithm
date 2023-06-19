46124ed103825d907b45b2f45fdef4962b4b60de
BladEO <amzi1118@gmail.com>
2023-06-19 22:04:50 +0900

    Baekjoon 2566

---
+++ Baekjoon/2566.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		int arr[9][9];
+		int max = 0, a = 0, b = 0;
+	
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				cin >> arr[i][j];
+			}
+		}
+
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (arr[i][j] > max)
+				{
+					max = arr[i][j];
+					a = i;
+					b = j;
+				}
+			}
+		}
+
+		cout << arr[a][b] << endl << a + 1 << " " << b + 1;
+}
